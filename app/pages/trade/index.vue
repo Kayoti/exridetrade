b517ffec6aa8755387df04e91c6635e73defd8ba
MainaGeofrey <mainageofrey731@gmail.com>
2024-12-20 10:51:14 +0300

    change

--- app/pages/trade/index.vue
+++ app/pages/trade/index.vue
@@ -29,27 +29,25 @@ const cta = {
 }
 
 const handleLogout = () => {
-  localStorage.removeItem("app_user")
+  localStorage.removeItem('app_user')
   firebaseLogout()
-  localStorage.removeItem("leadid")
+  localStorage.removeItem('leadid')
 
-  isSignOutLoading.value = true;
+  isSignOutLoading.value = true
 
   reloadNuxtApp({
-    path: "/",
-    ttl: 1000,
-  });
+    path: '/',
+    ttl: 1000
+  })
 }
 
-const handleAddVehicle= () => {
-
-  console.log("Button clicked! Performing action...");
-
+const handleAddVehicle = () => {
+  console.log('Button clicked! Performing action...')
 }
 
 onMounted(() => {
-  lead.value = localStorage.getItem("leadid")
-  store.$state.form = JSON.parse(localStorage.getItem("form") || '{}') // Parse the form data
+  lead.value = localStorage.getItem('leadid')
+  store.$state.form = JSON.parse(localStorage.getItem('form') || '{}') // Parse the form data
 })
 </script>
 
@@ -57,38 +55,60 @@ onMounted(() => {
   <div>
     <ULandingSection>
       <div class="flex flex-col md:flex-row gap-6">
-
         <dl class="flex-1 max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
           <div class="flex flex-col pb-3">
-            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">FirstName</dt>
-            <dd class="text-lg font-semibold">{{ store.$state.form.firstname || "-" }}</dd>
+            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
+              FirstName
+            </dt>
+            <dd class="text-lg font-semibold">
+              {{ store.$state.form.firstname || "-" }}
+            </dd>
           </div>
           <div class="flex flex-col pb-3">
-            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">LastName</dt>
-            <dd class="text-lg font-semibold">{{ store.$state.form.lastname || "-" }}</dd>
+            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
+              LastName
+            </dt>
+            <dd class="text-lg font-semibold">
+              {{ store.$state.form.lastname || "-" }}
+            </dd>
           </div>
           <div class="flex flex-col pt-3">
-            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Phone number</dt>
-            <dd class="text-lg font-semibold">{{ store.$state.form.phone ? `+${store.$state.form.phone}` : "-" }}</dd>
+            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
+              Phone number
+            </dt>
+            <dd class="text-lg font-semibold">
+              {{ store.$state.form.phone ? `+${store.$state.form.phone}` : "-" }}
+            </dd>
           </div>
           <div class="flex flex-col pb-3">
-            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Email address</dt>
-            <dd class="text-lg font-semibold">{{ store.$state.form.email || "-" }}</dd>
+            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
+              Email address
+            </dt>
+            <dd class="text-lg font-semibold">
+              {{ store.$state.form.email || "-" }}
+            </dd>
           </div>
           <div class="flex flex-col py-3 border-b border-gray-300 dark:border-gray-600">
-            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Address</dt>
-            <dd class="text-lg font-semibold">{{ store.$state.form.location || "-" }}</dd>
+            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
+              Address
+            </dt>
+            <dd class="text-lg font-semibold">
+              {{ store.$state.form.location || "-" }}
+            </dd>
           </div>
-          <UButton :loading="isSignOutLoading"
+          <UButton
+            :loading="isSignOutLoading"
             class="border border-gray-400 text-black hover:bg-gray-100 focus:outline-none font-medium text-sm px-5 py-2.5 text-center inline-flex justify-center items-center dark:focus:outline-none bg-white w-full max-w-xs mt-4"
-            @click="handleLogout()">
+            @click="handleLogout()"
+          >
             Sign Out
           </UButton>
         </dl>
 
-
-
-        <ULandingCTA v-bind="cta" class="flex-1 bg-gray-100/50 dark:bg-gray-800/50 p-6" />
+        <ULandingCTA
+          v-bind="cta"
+          class="flex-1 bg-gray-100/50 dark:bg-gray-800/50 p-6"
+        />
       </div>
     </ULandingSection>
   </div>

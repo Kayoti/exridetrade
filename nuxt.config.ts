9752bb436d42e493f9fbe505235332bcb24a414a
MainaGeofrey <mainageofrey731@gmail.com>
2025-01-02 19:44:56 +0300

    nuct config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,47 +11,48 @@ export default defineNuxtConfig({
     '@nuxthq/studio',
     '@vueuse/nuxt',
     'nuxt-og-image',
-        '@pinia/nuxt'
+    '@pinia/nuxt'
   ],
 
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton'].includes(c.pascalName))
+  /** *  routeRules: {
+    '/api/search.json': { prerender: true },
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  }, */
 
-      globals.forEach(c => c.global = true)
-    }
+  devtools: {
+    enabled: true
   },
 
   colorMode: {
     disableTransition: true
   },
 
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-07-11',
+
   nitro: {
     prerender: {
-      routes: [
-        '/',
-        '/docs'
-      ],
+      routes: ['/', '/docs'],
       crawlLinks: true
     }
-  },
-
-  routeRules: {
-    '/api/search.json': { prerender: true },
-    '/docs': { redirect: '/docs/getting-started', prerender: false }
-  },
-
-  devtools: {
-    enabled: true
   },
 
   typescript: {
     strict: false
   },
 
-  future: {
-    compatibilityVersion: 4
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c =>
+        ['UButton'].includes(c.pascalName)
+      )
+
+      globals.forEach(c => (c.global = true))
+    }
   },
 
   eslint: {
@@ -61,7 +62,5 @@ export default defineNuxtConfig({
         braceStyle: '1tbs'
       }
     }
-  },
-
-  compatibilityDate: '2024-07-11'
+  }
 })
